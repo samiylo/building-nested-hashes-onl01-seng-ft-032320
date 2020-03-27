d7615f2a42fbd95e9586696147b09efc075e450c
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 16:46:56 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,6 +20,7 @@ def fourth_challenge
         #your hashes here!
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
+        ]
       ]
    }
 }
