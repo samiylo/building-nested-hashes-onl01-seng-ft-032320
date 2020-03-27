0ef6ecc79ab23fe713bf577257b9f44666aed453
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 16:46:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         [
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      
       ]
    }, 
    :capulet => {
