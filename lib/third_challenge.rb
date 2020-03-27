0a67dfb22dca4a1513e507328e571ea3934c64ff
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 16:35:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -40,7 +40,8 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-        
+        {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"
         ]
    }
   }
